7bbb9c01511425347d3ddfa328a3692adc272b40
tai03102004 <124711101+tai03102004@users.noreply.github.com>
2024-05-19 14:47:33 +0700

    update

--- pet/controllers/client/checkout.controller.js
+++ pet/controllers/client/checkout.controller.js
@@ -5,28 +5,34 @@ const productHelper = require("../../helper/products");
 
 // [GET] /checkout
 module.exports.index = async(req,res) => {
-    const cartId = req.cookies.cartId;
+    try {
+        const cartId = req.cookies.cartId;
 
-    const cart = await Cart.findOne({
-        _id : cartId,
-    });
-    if (cart.products.length > 0) {
-        for (const item of cart.products) {
-            const productId = item.product_id;
-            const productInfo = await Product.findOne({
-                _id : productId,
-            })
-            // productInfo : Thông tin chó mà mình mua...
-            productInfo.priceNew = await productHelper.priceNewProduct(productInfo);
-            item.productInfo = productInfo;
-            item.totalPrice = item.quantity * productInfo.priceNew;
+        const cart = await Cart.findOne({
+            _id : cartId,
+        });
+        if (cart.products.length > 0) {
+            for (const item of cart.products) {
+                const productId = item.product_id;
+                const productInfo = await Product.findOne({
+                    _id : productId,
+                })
+                // productInfo : Thông tin chó mà mình mua...
+                productInfo.priceNew = await productHelper.priceNewProduct(productInfo);
+                item.productInfo = productInfo;
+                item.totalPrice = item.quantity * productInfo.priceNew;
+            }
         }
+        cart.totalPrice = cart.products.reduce((totalPrice, item) => totalPrice + item.totalPrice,0);
+        res.render("client/pages/checkout/index",{
+            pageTitle : "Đặt hàng",
+            cartDetail : cart,
+        })
     }
-    cart.totalPrice = cart.products.reduce((totalPrice, item) => totalPrice + item.totalPrice,0);
-    res.render("client/pages/checkout/index",{
-        pageTitle : "Đặt hàng",
-        cartDetail : cart,
-    })
+    catch(err) {
+        console.log(err);
+        res.redirect("back");
+    }
 }
 
 // [POST] /checkout/order
