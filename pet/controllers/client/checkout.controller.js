a5c5213bb4a29fad1badaf7c5194676a4af156bc
tai03102004 <124711101+tai03102004@users.noreply.github.com>
2024-05-19 14:46:18 +0700

    update

--- pet/controllers/client/checkout.controller.js
+++ pet/controllers/client/checkout.controller.js
@@ -32,47 +32,53 @@ module.exports.index = async(req,res) => {
 // [POST] /checkout/order
 
 module.exports.order = async(req,res) => {
-    const cartId = req.cookies.cartId;
-    const userInfo = req.body;
+    try {
+        const cartId = req.cookies.cartId;
+        const userInfo = req.body;
 
-    const cart = await Cart.findOne({
-        _id : cartId,
-    });
+        const cart = await Cart.findOne({
+            _id : cartId,
+        });
 
-    let products = [];
+        let products = [];
 
-    for (const product of cart.products) {
-        const objectProduct = {
-            product_id : product.product_id,
-            price : 0,
-            discountPercentage : 0,
-            quantity : product.quantity,
+        for (const product of cart.products) {
+            const objectProduct = {
+                product_id : product.product_id,
+                price : 0,
+                discountPercentage : 0,
+                quantity : product.quantity,
+            };
+
+            const productInfo = await Product.findOne({
+                _id : product.product_id
+            });
+            objectProduct.price = productInfo.price;
+            objectProduct.discountPercentage = productInfo.discountPercentage;
+
+            products.push(objectProduct);
+        }
+
+        const objectOrder = {
+            cart_id : cartId,
+            userInfo : userInfo,
+            products : products
         };
 
-        const productInfo = await Product.findOne({
-            _id : product.product_id
-        });
-        objectProduct.price = productInfo.price;
-        objectProduct.discountPercentage = productInfo.discountPercentage;
-
-        products.push(objectProduct);
+        const order = new Order(objectOrder);
+        await order.save();
+        // Sau khi lưu products vào Order => success => gán products = [] => Lưu sp khác
+        await Cart.updateOne({
+            _id : cartId,
+        },{
+            products : []
+        })
+        res.redirect(`/checkout/success/${order.id}`);
+    } catch (err) {
+        console.log(err);
+        res.redirect("back");
     }
-
-    const objectOrder = {
-        cart_id : cartId,
-        userInfo : userInfo,
-        products : products
-    };
-
-    const order = new Order(objectOrder);
-    await order.save();
-    // Sau khi lưu products vào Order => success => gán products = [] => Lưu sp khác
-    await Cart.updateOne({
-        _id : cartId,
-    },{
-        products : []
-    })
-    res.redirect(`/checkout/success/${order.id}`);
+    
 }
 
 // [GET] / checkout/success/:orderId
